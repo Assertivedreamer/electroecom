95660c3343062a2e7536a5fe2a271e445fc43dae
JeuriMorel <rikonikage@yahoo.com>
2021-09-30 11:14:46 -0400

    added mobile/tablet nav. turned logo to home link

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,3 +1,25 @@
-document.addEventListener('DOMContentLoaded', () => {
-    console.log('loaded')
-})
+const body = document.querySelector('body');
+const hamburgerBtn = document.querySelector('.header__hamburger-toggle');
+const hamburgerImg = document.querySelector('.header__hamburger')
+const mobileNav = document.querySelector('.categories--nav')
+
+
+
+function handleHam(){
+    body.classList.toggle('nav-is-open')
+    mobileNav.classList.toggle('nav-is-open')
+    hamburgerImg.classList.toggle('nav-is-open')
+
+}
+
+
+//EVENT LISTENERS
+hamburgerBtn.addEventListener('click', handleHam)
+
+
+
+
+
+// document.addEventListener('DOMContentLoaded', () => {
+//     console.log('loaded')
+// })
